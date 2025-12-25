f73dca5c4c2c90d80df2e729fb1acb50237d1989
dev-760 <148582551+dev-760@users.noreply.github.com>
2025-12-25 21:04:12 +0100

    modified:   src/index.css 	modified:   src/vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,8 +4,4 @@
 interface ImportMetaEnv {
     readonly VITE_SUPABASE_URL: string
     readonly VITE_SUPABASE_ANON_KEY: string
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv
-}
+    readonly VITE_SUPABASE_PROJECT_REF: string
